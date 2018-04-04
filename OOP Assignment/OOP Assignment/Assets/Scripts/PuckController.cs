44f9cdb2fc92914578db8f8809511dbeef5f599a
SkyBox\Azure Skies <azzhayman@gmail.com>
2018-04-04 22:58:09 +1000

    Assignment draft upload

---
+++ OOP Assignment/OOP Assignment/Assets/Scripts/PuckController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace OOPAssignment
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PuckController : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
